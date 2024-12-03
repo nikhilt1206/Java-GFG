401fa35da1f4bdf99d09bbd0582c5d77389558f2
Nikhil Tiwari <nikhilt1206@gmail.com>
2024-12-03 22:15:29 +0530

    Added new file - Added new package for Variable and Data Types

---
+++ Java_Codes/src/Variables_Data_Types/Variable_and_data_types.java
@@ -0,0 +1,28 @@
+package Variables_Data_Types;
+import java.util.*;
+
+public class Variable_and_data_types {
+
+    public static void main(String[] args){
+        int age =29;
+        String name = "nikhil";
+        float weight = 65.5f;
+        boolean isValid = true;
+        byte marks = 90;
+        float pi = 3.14f;
+        float div =15.0f/4.0f;
+        long views = 1000000000000000000L;
+        char gender ='N';
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
+        System.out.println(isValid);
+        System.out.println(marks);
+        System.out.println(pi);
+        System.out.println(div);
+        System.out.println(views);
+        System.out.println(gender);
+
+    }
+}
