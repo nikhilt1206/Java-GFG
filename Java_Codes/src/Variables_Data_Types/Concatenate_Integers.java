9726098ea0e9355f7de6e547d62d8b3818cc5c78
Nikhil Tiwari <nikhilt1206@gmail.com>
2024-12-08 18:42:34 +0530

    Added new file - Added new code Problems

---
+++ Java_Codes/src/Variables_Data_Types/Concatenate_Integers.java
@@ -0,0 +1,10 @@
+package Variables_Data_Types;
+import java.util.*;
+public class Concatenate_Integers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        System.out.print(a+""+b);
+    }
+}
---
+++ Java_Codes/src/Variables_Data_Types/Type_Conversion.java
@@ -0,0 +1,10 @@
+package Variables_Data_Types;
+import java.util.*;
+public class Type_Conversion {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        int num = (int)d;
+        System.out.print(num);
+    }
+}
