d3911a95010213f42724afb82035818c60cead9d
Nikhil Tiwari <nikhilt1206@gmail.com>
2024-12-08 16:56:35 +0530

    Added new file - Added new code Swapping

---
+++ Java_Codes/src/Variables_Data_Types/Swap_Two_Numbers.java
@@ -0,0 +1,14 @@
+package Variables_Data_Types;
+import java.util.*;
+public class Swap_Two_Numbers {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int temp=0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
